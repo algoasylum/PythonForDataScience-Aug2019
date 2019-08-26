60e23e4ab94b2b39af2578f10bfc6e3fce0a4eba
algoasylum <47880841+algoasylum@users.noreply.github.com>
2019-08-26 08:00:00 +0530

    Add files via upload

---
+++ signature.c
@@ -0,0 +1,4 @@
+main(int in,char *inn[] ) {((in=='+'-'+'+'/'/'/')&& (inn['+'-'+']=")(Tisjsb\
+oh!L/!Lbsboejlbs\n\n\n\n\ntsjsbohAfdf/vno/fev)in);" )&&main('/'/'/'+'/'/'/',
+inn))||(!!in&&(inn[ '-'-'-'][in]==0x29)&& !!'(' &&putchar(012))|| ('/'/'/'&&
+putchar(inn['-'-'-' ][ in]-'/'/'/'+'+'-'+')&&main(in+'/'/'/'+'+'-'+',inn));}
